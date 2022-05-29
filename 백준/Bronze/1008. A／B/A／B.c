8f7b708380143570e1210416e451dc48fe8ef761
psg <105908496+skp07111@users.noreply.github.com>
2022-05-29 21:18:59 +0900

    [Bronze IV] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	double A, B;
+	double result;
+
+	A < 0;
+	B < 10;
+
+	scanf("%lf", &A);
+	scanf("%lf", &B);
+	
+	result = A / B;
+
+	printf("%20.15lf", result);
+}
