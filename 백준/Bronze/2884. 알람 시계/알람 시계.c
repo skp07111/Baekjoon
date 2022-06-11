224f717837f622128487b584c3d59fdb0424232f
skp <105908496+skp07111@users.noreply.github.com>
2022-06-11 14:04:01 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int h, m;
+    scanf("%d %d", &h, &m);
+    
+    if(m < 45) {
+        m = m + 15;
+        if (h == 0) {
+            h = 23;
+        }
+        else
+            h = h - 1;
+    }
+    else {
+        m = m - 45;
+    }
+    
+    printf("%d %d", h, m);
+}
