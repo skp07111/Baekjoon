e82cfecbb293333dc871b6ad4dbc3d5a29a0df76
skp <105908496+skp07111@users.noreply.github.com>
2022-06-17 00:49:39 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	A >= 1;
+	B <= 10000;
+
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+	
+	printf("%d \n", A + B);
+	printf("%d \n", A - B);
+	printf("%d \n", A * B);
+	printf("%d \n", A / B);
+	printf("%d \n", A % B);
+}
