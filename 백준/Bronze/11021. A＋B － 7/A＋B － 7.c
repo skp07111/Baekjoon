00d498745a89fc6c686d0f49ac3a63c8e6766d69
NaCl <105908496+skp07111@users.noreply.github.com>
2022-06-19 00:31:58 +0900

    [Bronze III] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    int A[T], B[T];
+    for (int i=0; i<T; i++) {
+        scanf("%d %d", &A[i], &B[i]);
+        printf("Case #%d: %d\n", i+1, A[i]+B[i]);
+    }
+}
