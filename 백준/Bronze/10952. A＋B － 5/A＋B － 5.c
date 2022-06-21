4cb5b1cdb5b11cef7fa3300a73ce3743a3fc3671
NaCl <105908496+skp07111@users.noreply.github.com>
2022-06-22 01:12:18 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int A = 0, B = 0;
+   while(A >= 0 && B >= 0) {
+       scanf("%d %d", &A, &B);
+       if(A == 0 && B == 0) break;
+       printf("%d\n", A + B);
+    }
+}
