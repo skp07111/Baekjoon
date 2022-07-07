dab6ec44fb4bd47cd1942cd6eb63f1112f204c21
NaCl <105908496+skp07111@users.noreply.github.com>
2022-07-08 05:03:31 +0900

    [Bronze III] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -3,7 +3,7 @@
 int main() {
     int T, A, B;
     scanf("%d", &T);
-    for (int i=1; i<=T; i++) {
+    for (int i = 1; i <= T; i++) {
         scanf("%d %d", &A, &B);
         printf("%d \n", A + B);
     }
