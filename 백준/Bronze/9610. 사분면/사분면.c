fdbae3cb46d199521a6b3b8d7e383f4e1e18c92b
NaCl <105908496+skp07111@users.noreply.github.com>
2022-08-23 00:43:13 +0900

    [Bronze III] Title: 사분면, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, q1 = 0, q2 = 0, q3 = 0, q4 = 0, axis = 0;
+    scanf("%d", &num);
+    for(int i = 0; i < num; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        if (x > 0 && y > 0) q1++;
+        else if (x < 0 && y > 0) q2++;
+        else if (x < 0 && y < 0) q3++;
+        else if (x > 0 && y < 0) q4++;
+        else axis++;
+    }
+    printf("Q1: %d\n", q1);
+    printf("Q2: %d\n", q2);
+    printf("Q3: %d\n", q3);
+    printf("Q4: %d\n", q4);
+    printf("AXIS: %d\n", axis);
+}
