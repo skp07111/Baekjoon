beda06c59baea39a97c9a2bf17cec20ae29ac03e
NaCl <105908496+skp07111@users.noreply.github.com>
2023-02-02 01:40:56 +0900

    Update ATM.py

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,14 @@
+# 사람의 수 n 입력
 n = int(input())
-data = list(map(int, input().split()))
+# 각 사람이 돈을 인출하는데 걸리는 시간을 입력받아 data 리스트에 저장
+data = list(map(int, input().split()))  
+# data 리스트 오름차순 정렬
 data.sort()
+
 sum = 0
 for i in range(n+1):
     for j in range(i):
-        sum += data[j]
+        sum += data[j]  # 오름차순으로 정렬된 값들 차례대로 더하기
+        
+# 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값 sum 출력
 print(sum)
